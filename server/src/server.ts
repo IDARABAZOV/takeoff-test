afe86edd9fb7c8508635faa22fcca7061d332689
Abazov Idar <idar112@gmail.com>
2022-06-30 19:00:20 +0300

    настройка авторизации на сервере

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,17 @@
-import {IncomingMessage, ServerResponse} from "http";
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+const server = jsonServer.create()
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'database/db.json'))
+const middlewares = jsonServer.defaults()
 
-const http = require('http')
+server.db = router.db
 
-const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    res.write("hello world")
-    res.end()
+server.use(auth)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 5000
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on server: ${PORT}`)
 })
-
-server.listen(3001)
